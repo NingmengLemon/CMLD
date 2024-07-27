89e03219ec255ce82322329230a8f70cdcf22d89
NingmengLemon <guanchenxiang@outlook.com>
2024-07-27 13:25:46 +0800

    remove pack.cmd

--- pack.cmd
+++
@@ -1,6 +0,0 @@
-@cd %~dp0
-@pipenv install --skip-lock
-@pipenv run pip install pyinstaller
-@pipenv run pip install -r requirements.txt
-@pipenv run pyinstaller -i "./favicon.ico" --noconfirm --onefile "./CMLD.py"
-@pause
